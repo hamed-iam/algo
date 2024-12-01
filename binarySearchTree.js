ac06a13aded51f430e3570561c7f47926dcd68a8
Hamed Ghazali <hamed.ghazali@digikala.com>
2024-12-01 09:18:59 +0330

    more

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -78,6 +78,24 @@ class BinarySearchTree {
       console.log(root.value);
     }
   }
+
+  // BFS (breadth first search)
+  levelOrder() {
+    // use optimized queue class
+    const queue = [];
+    queue.push(this.root);
+
+    while (queue.length) {
+      let curr = queue.shift();
+      console.log(curr.value);
+      if (curr.left) {
+        queue.push(curr.left);
+      }
+      if (curr.right) {
+        queue.push(curr.right);
+      }
+    }
+  }
 }
 
 const bst = new BinarySearchTree();
--- dataStructures.js
+++ dataStructures.js
@@ -202,3 +202,43 @@ list.append(30);
 console.log("list is empty", list.isEmpty());
 console.log("list size", list.getSize());
 list.print();
+
+class HashTable {
+  constructor(size) {
+    this.table = new Array(size);
+    this.size = size;
+  }
+
+  hash(key) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+      total += key.charCodeAt(i);
+    }
+    return total % this.size;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    this.table[index] = value;
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    return this.table[index];
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    this.table[index] = undefined;
+  }
+
+  display() {
+    for (let i = 0; i < this.table.length; i++) {
+      if (this.table[i]) {
+        console.log(i, this.table[i]);
+      }
+    }
+  }
+}
+
+const table = new HashTable(50);
