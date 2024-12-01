c050243f51c864afc8de4c34284d564c7beecf8b
Hamed Ghazali <hamed.ghazali@digikala.com>
2024-12-01 09:09:48 +0330

    more

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-import { bubbleSort } from "./sorting.js";
-console.log(bubbleSort([8, 20, -2, 4, -6]));
+import {
+  cartesianProdict,
+  climbStairs,
+  climbStairsIterative,
+  towerOfHanoi,
+} from "./misc.js";
+import { bubbleSort, insertionSort, mergeSort, quickSort } from "./sorting.js";
+const arr = [8, 20, -2, 4, -6];
+console.log("bubble sort", bubbleSort(arr));
+console.log("insertionSort", insertionSort(arr));
+console.log("quicksort", quickSort(arr));
+console.log("mergesort", mergeSort(arr));
+
+console.log("cartesian product", cartesianProdict([1, 2], [3, 4]));
+console.log("cartesian product", cartesianProdict([1, 2], [3, 4, 5]));
+console.log("climb stairs", climbStairs(5));
+console.log("climb stairs", climbStairs(4));
+console.log("climb stairs", climbStairs(3));
+
+console.log("climb stairs iterative", climbStairsIterative(5));
+console.log("climb stairs iterative", climbStairsIterative(4));
+console.log("climb stairs iterative", climbStairsIterative(3));
+
+console.log("tower of hanoi", towerOfHanoi(4, "A", "B", "C"));
--- sorting.js
+++ sorting.js
@@ -14,3 +14,55 @@ export function bubbleSort(arr) {
   } while (swapped);
   return arr;
 }
+
+export function insertionSort(arr) {
+  for (let i = 1; i < arr.length - 1; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = numberToInsert;
+  }
+  return arr;
+}
+
+export function quickSort(arr) {
+  if (arr.length < 2) return arr;
+  let pivot = arr[arr.length - 1];
+  let right = [];
+  let left = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > pivot) {
+      right.push(arr[i]);
+    } else {
+      left.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+export function mergeSort(arr) {
+  if (arr.length < 2) return arr;
+  let middle = Math.floor(arr.length / 2);
+
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let sortedArr = [];
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+  return [...sortedArr, ...left, ...right];
+}
